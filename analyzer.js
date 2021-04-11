d04b79e7494aa553a5e5a9a3df82f3bb66b1fe82
pantchayan <pantchayan@gmail.com>
2021-04-11 12:57:48 +0530

    initial commit

---
+++ analyzer.js
@@ -0,0 +1,34 @@
+let puppeteer = require("puppeteer");
+let fs = require("fs");
+// no of videos 
+// views 
+// watch time -> get 
+// list of videos -> [name, duration]
+// initial page data get 
+// handle -> loader
+
+console.log("Before");
+// let arr=document.querySelectorAll("#stats  .style-scope.ytd-playlist-sidebar-primary-info-renderer")
+// let playlistArr=[]
+// playlistArr.push(arr[0].innerText,arr[1].innerText)
+
+// let watchTimeArr = document.querySelectorAll(".style-scope ytd-thumbnail-overlay-time-status-renderer");
+// let videosNameArr = document.querySelectorAll("#video-title");
+
+(async function () {
+    try {
+        let browserInstance = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null,
+            args: ["--start-maximized"]
+        });
+        let newPage = await browserInstance.newPage();
+        await newPage.goto("https://www.youtube.com/playlist?list=PLRBp0Fe2GpgnIh0AiYKh7o7HnYAej-5ph");
+        // evaluate
+       
+
+    } catch (err) {
+        console.log(err);
+    }
+
+})();
